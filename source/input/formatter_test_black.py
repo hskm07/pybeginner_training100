5e943b0cbd3bebe6c87c35aeb1dc38a8aaa9f1b7
hsono10n1 <hsono10n1@gmail.com>
2022-10-16 08:17:36 +0900

    changed: 問題135,136,137で必要となるファイルを追加

---
+++ source/input/formatter_test_black.py
@@ -0,0 +1,10 @@
+import os
+import abc
+from numpy import *
+
+
+
+a=1+1
+    
+print(a)
+print("This test script.All users of Flake8 should read this portion of the documentation.")
---
+++ source/input/lint_test_flake8.py
@@ -0,0 +1,4 @@
+a = 1 + 1
+print(a)
+print("This test script.All users of Flake8 should read\
+        this portion of the documentation.")
